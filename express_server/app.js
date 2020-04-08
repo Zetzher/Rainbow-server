95eb3174b5105894224f70d976772ff92124e0fa
Lou08 <l.casals86@gmail.com>
2020-04-08 12:35:37 +0200

    fallo de Rotuer

--- express_server/app.js
+++ express_server/app.js
@@ -1,4 +1,4 @@
-//require('dotenv').config();
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -9,25 +9,39 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 const bcrypt = require('bcrypt');
-const multer = require ('multer');
-const favicon = require ('serve-favicon')
+const multer = require('multer');
+const favicon = require('serve-favicon')
 
-mongoose.connect('mongodb://localhost:27017/rainbow',{
+
+mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
-  })
-  .then(x => {
-    console.log(
-      `Connected to Mongo! Database name: "${x.connections[0].name}"`
-    );
-  })
-  .catch(err => {
-    console.error('Error connecting to mongo', err);
-  });
+})
+    .then(x => {
+        console.log(
+            `Connected to Mongo! Database name: "${x.connections[0].name}"`
+        );
+    })
+    .catch(err => {
+        console.error('Error connecting to mongo', err);
+    });
+const app = express()
 
 
-  
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
+var index = require('./routes/index');
+app.use('/', index);
+
+
+module.exports = router;
+module.exports = app;
 
