5759d24d093979bf8ba75f5b0df60723c0862f7e
Zetzher <fyeah16@gmail.com>
2020-04-08 16:20:42 +0200

    j

--- express_server/models/user.js
+++ express_server/models/user.js
@@ -2,11 +2,9 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const user = new Schema({
 
-    username:
-    {
+    username: {
         type: String,
         unique: true,
-        required: true,
     },
     password: {
         type: String,
