fdd7b52fa4661b6902d32825f5cd30b030f6ca40
Lou08 <l.casals86@gmail.com>
2020-04-09 15:44:23 +0200

    profile ok

--- express_server/routes/perfil.js
+++ express_server/routes/perfil.js
@@ -1,15 +1,41 @@
-// const express = require("express");
-// const router = express.Router();
-// const createError = require("http-errors");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
 
-// router.get('profile', (req, res, next) => {
-//     const userId = req.session.currentUser._id
 
-//     User
-//         .findById(userId)
-//         .then(user=>{
+//POST
+router.get('/', (req,res,next)=>{
+const userId = req.session.currentUser._id
+//console.log('session :', req.session.currentUser._id);
+    User
+    .findById(userId)
+    .then(dbResponse => {
+        console.log(dbResponse)
+        res.status(200)
+        .json(dbResponse)
+    }
+    )
+    .catch(err => console.log('En metodo GET de profile ha ocurrido:', err))
+})
 
-//         })
-// })
+//EDIT PERFIL
+router.post('/edit/:id', async (req, res, next) => {
+	try {
+		//console.log(req.body);
+		const {username, nombre,apellido,edad,email, photo_url} = req.body;
+		console.log(req.body)
+		//const profilePicture = req.file ? req.file.secure_url : req.session.currentUser.photo_url;
+		await User.findByIdAndUpdate({
+            _id: req.params.id
+            
+		}, { username, nombre,apellido,edad,email, photo_url});
+        
+            res.status(200)
+            .json({username, nombre,apellido,edad,email, photo_url})
+        
+	} catch (error) {
+		//console.log(error);
+	}
+});
+
+module.exports = router;
